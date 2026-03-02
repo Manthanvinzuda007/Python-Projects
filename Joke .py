1df9caa4fb51a9050b2ab7e0d4efc017f01e9137
Manthan Vinzuda <206708551+Manthanvinzuda007@users.noreply.github.com>
2026-03-02 15:20:04 +0530

    Project

---
+++ Joke .py
@@ -0,0 +1,19 @@
+import random
+import time
+
+jokes = [
+    "Why do programmers prefer dark mode? Because light attracts bugs! 🐛😂",
+    "Why did Python break up with Java? Because Python found indentation more meaningful 😜",
+    "Programmer nu favourite place? GitHub, kemke tya badhu 'commit' thay chhe 🤣",
+    "Why was the computer cold? Because it forgot to close Windows 🪟❄️",
+    "Python developer gym kem nathi jato? Kemke ene already enough 'loops' mali gaya chhe 😆"
+]
+
+print("🤣 Joke Generator Loading...")
+time.sleep(1)
+
+print("\nReady? Aavto joke...\n")
+time.sleep(1)
+
+print(random.choice(jokes))
+                                                     # Created By Manthan Vinzuda....
